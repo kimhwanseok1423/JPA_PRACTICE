d74ada3dc989c3bac7042663a825559b51d1e087
Hwan seok Kim <kcsss4499@gamil.com>
2024-08-15 23:40:47 +0900

    .Booleanbuilder를 활용한 동적쿼리 회원검색기능 개발

---
+++ src/main/java/jpabook/jpashop/dto/UserDto.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.dto;
+
+public class UserDto {
+private String name;
+private int age;
+
+    public UserDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
